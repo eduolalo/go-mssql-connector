9b4fca5d2422bad67e0118697985b135d12f9698
Eduardo Rodriguez Cisneros <rcchristiane@gmail.com>
2021-02-01 17:03:59 -0500

    add null unique identifier

--- UniqueIdentifier.go
+++
@@ -1,9 +0,0 @@
-package mssql
-
-import mssqldb "github.com/denisenkom/go-mssqldb"
-
-// UniqueIdentifier es una extend del paquete de denisenkompara que esté
-// disponible sin reimportar el pquete
-type UniqueIdentifier struct {
-	mssqldb.UniqueIdentifier
-}
